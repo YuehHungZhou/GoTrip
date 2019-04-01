991cdaad7e8a77b0a3d31a2bc4dc7617a3d5390b
topdsr2 <topdsr22@hotmail.com>
2019-04-01 18:03:18 +0800

    create

---
+++ app/src/main/java/com/topdsr2/gotrip/MainActivity.java
@@ -0,0 +1,13 @@
+package com.topdsr2.gotrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
